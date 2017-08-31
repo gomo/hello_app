53812f6adcef87c39142d02fe64cb9fcf06c26da
Masamoto Miyata <miyata@sincere-co.com>
2017-08-31 02:24:41 +0000

    Added goodbye action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "¡Hola, mundo!日本語"
+    render html: "¡Hola,世界"
+  end
+  
+  def goodbye
+    render html: "goodbye 世界"
   end
 end
